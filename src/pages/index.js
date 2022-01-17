3ae6d0aa85461fb8375e5d4d08cdd0cbc7449f00
Sanjeivk <sanjeiv411@gmail.com>
2022-01-17 11:03:47 -0500

    Used plugins to add images to the Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import AboutPage from './about';
 import Layout from './layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const HomePage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Home Page for Gatsby Website</p>
+      <StaticImage alt="Dubai" src="../images/dub.jpeg" />
     </Layout>
   );
 };
